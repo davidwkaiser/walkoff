e0738e0ff2bf9908530d36602c6e1411395453cc
Henry Firth <henry@bellwethermade.com>
2017-03-25 13:39:37 -0500

    Add Game#potential_walkoff? method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,15 @@
 require 'mlb_gameday'
 
 class Game < ApplicationRecord
+  def potential_walkoff?
+    api = MLBGameday::API.new
+    game = api.game(self.gid)
+    inning = game.inning
+    score_diff = game.score.first - game.score.last
+    num_runners = game.runners.compact.count
+
+    inning.first >=9 && inning.last == "bottom" && score_diff < num_runners + 1
+  end
 
   def self.get_state(game)
     if game.started?
