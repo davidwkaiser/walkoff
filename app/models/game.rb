20c7f4dd83e2098ec6eed5a9ff9828139dde81f9
Henry Firth <henry@bellwethermade.com>
2017-03-25 13:24:22 -0500

    Add Game class methods for population

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,24 @@
+require 'mlb_gameday'
+
 class Game < ApplicationRecord
+
+  def self.get_state(game)
+    if game.started?
+      if game.in_progress?
+        "IN PROGRESS"
+      else
+        "POSTGAME"
+      end
+    else
+      "PREGAME"
+    end
+  end
+
+  def self.populate
+    api = MLBGameday::API.new
+    games = api.find_games
+    games.map do |game|
+      Game.create(gid: game.gid, state: get_state(game))
+    end
+  end
 end
